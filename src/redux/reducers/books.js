acf732741ddd341850c317f62575785b4ea7e8fd
Rüdiger zu Dohna <snackbox@sinntr.eu>
2019-02-11 06:11:01 +0100

    extract updateBook

--- src/redux/reducers/books.js
+++ src/redux/reducers/books.js
@@ -1,23 +1,29 @@
 import {DEC, INC, RECEIVE_BOOKS} from "../actionTypes";
 
+function updateBook(book, action) {
+    switch (action.type) {
+        case INC:
+            return book.author = book.author + "x";
+        case DEC:
+            return book.author = book.author.substring(0, book.author.length - 1);
+        default:
+            console.error("unknown book action", action);
+    }
+}
+
 export default function books(books = [], action) {
     console.log("booksReducer", books, action);
 
-    const bookWith = update => {
-        return books.map((book) => {
-            if (book.id === action.payload.id) {
-                const copy = {...book};
-                update(copy);
-                return copy;
-            } else return book;
-        });
-    };
-
     switch (action.type) {
         case INC:
-            return bookWith(book => book.author = book.author + "x");
         case DEC:
-            return bookWith(book => book.author = book.author.substring(0, book.author.length - 1));
+            return books.map((book) => {
+                if (book.id === action.payload.id) {
+                    const copy = {...book};
+                    updateBook(copy, action);
+                    return copy;
+                } else return book;
+            });
         case RECEIVE_BOOKS:
             return action.payload;
         default:
