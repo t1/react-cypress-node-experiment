b493220b465118fd822a061b4471250a5609f15f
Rüdiger zu Dohna <snackbox@sinntr.eu>
2019-02-14 09:29:23 +0100

    use `Link` to navigate back to prevent reload

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -5,13 +5,19 @@ import Breadcrumb from "react-bootstrap/Breadcrumb";
 
 import {faAngleLeft as AngleLeftIcon} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon as Icon} from "@fortawesome/react-fontawesome";
+import Link from "react-router-dom/es/Link";
 
 const BookDetail = ({book}) => {
     return (
         book ? (
             <Container>
                 <Breadcrumb>
-                    <Breadcrumb.Item href="/"><Icon icon={AngleLeftIcon} size="xs"/>Back To List</Breadcrumb.Item>
+                    <Breadcrumb.Item>
+                        <Link to={'/'}>
+                            <Icon icon={AngleLeftIcon} size="xs"/>
+                            Back To List
+                        </Link>
+                    </Breadcrumb.Item>
                 </Breadcrumb>
                 <h3>{book.author}: {book.title}</h3>
                 <small>ID:{book.id}</small>
