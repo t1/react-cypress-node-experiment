735e1d16affeebe614d7e581f5331447d2902c31
Rüdiger zu Dohna <snackbox@sinntr.eu>
2019-02-21 05:54:05 +0100

    use chai-url

--- cypress/integration/book-store-spec.js
+++ cypress/integration/book-store-spec.js
@@ -1,7 +1,11 @@
+import chaiUrl from 'chai-url'
+
+chai.use(chaiUrl);
+
 describe('Book Store Test', function () {
     it('Navigates the Book Store', function () {
         cy.visit('/');
-        cy.url().should('match', /\/$/);
+        cy.url().should('have.path', "/");
         cy.get('#counter').contains('0');
 
         cy.get('#counter-buttons\\+').click();
@@ -9,14 +13,14 @@ describe('Book Store Test', function () {
         cy.contains('J.R.R. Tolkien: The Lord Of The Rings');
         cy.get('#book-2\\+').click();
         cy.contains(`J.R.R. Tolkien${'x'}: The Lord Of The Rings`).click();
-        cy.url().should('match', /\/books\/2$/);
+        cy.url().should('have.path', "/books/2");
         cy.contains('J.R.R. Tolkien: The Lord Of The Rings');
 
         cy.get('#counter-buttons\\+').click();
         cy.get('#counter').contains('2');
 
         cy.contains('Back To List').click(); // cy.go('back');
-        cy.url().should('match', /\/$/);
+        cy.url().should('have.path', "/");
         cy.get('#counter').contains('2');
         cy.contains(`J.R.R. Tolkien${'x'}: The Lord Of The Rings`);
     })
